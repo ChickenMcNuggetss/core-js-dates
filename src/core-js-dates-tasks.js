08eb57bdfbeb8f775d7af63c17936632acde1c95
KarinaI <7karina.ibr@gmail.com>
2024-03-21 00:41:03 +0300

    feat: add solution

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -90,8 +90,10 @@ function getNextFriday(date) {
  * 1, 2024 => 31
  * 2, 2024 => 29
  */
-function getCountDaysInMonth(/* month, year */) {
-  throw new Error('Not implemented');
+function getCountDaysInMonth(month, year) {
+  let num = month;
+  num -= 1;
+  return new Date(year, num + 1, 0).getDate();
 }
 
 /**
